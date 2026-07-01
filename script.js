1b0dfe0d481a567963e8d71fde46c064fecc8adf
Ujjawal Sharma <ujjawalsharma7033@gmail.com>
2026-07-01 14:27:16 +0530

    Fix image paths

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /* =========== ASSETS =========== */
 const PROFILE_IMG = 'assets/profile.png';
-const ESQUE_IMG = 'assets/esque.png';
+const ESQUE_IMG = 'assets/Esque.png';
 const SEMBLE_IMG = 'assets/semble.png';
 const IRA_IMG = 'assets/ira.png';
-const AVRUP_IMG = 'assets/avrup.png';
+const AVRUP_IMG = 'assets/Avrup.png';
 const MLMETHOD_IMG = 'assets/mlmethod.png';
-const SUNCLUB_IMG = 'assets/sunclub.png';
-const RESUME_PDF = 'assets/Ujjawal_Sharma_Resume.pdf';
+const SUNCLUB_IMG = 'assets/Sunclub.png';
+const RESUME_PDF = 'assets/Ujjawal_Sharma_CV.pdf';
 
 const setSrc = (id, value) => {
   const el = document.getElementById(id);
